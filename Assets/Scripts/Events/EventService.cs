332148bd93649679dadbd6e8a5ee7794d8dd17c8
shobhit-rawat <sbtrwt.se@gmail.com>
2024-07-25 20:08:29 +0530

    Map added

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -5,7 +5,6 @@ namespace TowerDefense.Events
     public class EventService
     {
         public EventController<int> OnMapSelected { get; private set; }
-
         public EventService()
         {
             OnMapSelected = new EventController<int>();
---
+++ Assets/Scripts/Map/MapService.cs
@@ -0,0 +1,59 @@
+using TowerDefense.Events;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace TowerDefense.Map
+{
+    public class MapService
+    {
+        private MapSO mapScriptableObject;
+        private SpriteRenderer tileOverlay;
+        private MapData currentMapData;
+        private Grid currentGrid;
+        private Tilemap currentTileMap;
+
+        private EventService eventService;
+        public MapService(MapSO mapScriptableObject)
+        {
+            this.mapScriptableObject = mapScriptableObject;
+            tileOverlay = Object.Instantiate(mapScriptableObject.TileOverlay).GetComponent<SpriteRenderer>();
+            ResetTileOverlay();
+            LoadMap(1);
+        }
+        public void Init(EventService eventService)
+        {
+            this.eventService = eventService;
+            SubscribeToEvents();
+        }
+        private void SubscribeToEvents() => eventService.OnMapSelected.AddListener(LoadMap);
+        private void LoadMap(int mapId)
+        {
+            currentMapData = mapScriptableObject.MapDatas.Find(mapData => mapData.MapID == mapId);
+            currentGrid = Object.Instantiate(currentMapData.MapPrefab);
+            currentTileMap = currentGrid.GetComponentInChildren<Tilemap>();
+        }
+        private void ResetTileOverlay() => SetTileOverlayColor(TileOverlayColor.TRANSPARENT);
+
+        private void SetTileOverlayColor(TileOverlayColor colorToSet)
+        {
+            switch (colorToSet)
+            {
+                case TileOverlayColor.TRANSPARENT:
+                    tileOverlay.color = mapScriptableObject.DefaultTileColor;
+                    break;
+                case TileOverlayColor.SPAWNABLE:
+                    tileOverlay.color = mapScriptableObject.SpawnableTileColor;
+                    break;
+                case TileOverlayColor.NON_SPAWNABLE:
+                    tileOverlay.color = mapScriptableObject.NonSpawnableTileColor;
+                    break;
+            }
+        }
+        private enum TileOverlayColor
+        {
+            TRANSPARENT,
+            SPAWNABLE,
+            NON_SPAWNABLE
+        }
+    }
+}
